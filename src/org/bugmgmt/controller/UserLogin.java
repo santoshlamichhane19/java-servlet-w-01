b9a4575833e12a21c36352733e2264d05931cd63
u123456 <iamsan@iamsans-MacBook-Pro.local>
2020-02-24 18:15:44 -0800

    Project upload

---
+++ src/org/bugmgmt/controller/UserLogin.java
@@ -0,0 +1,28 @@
+package org.bugmgmt.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "login", urlPatterns = { "/login" })
+public class UserLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String username = request.getParameter("username");
+		System.out.println(username);
+
+	}
+
+}
